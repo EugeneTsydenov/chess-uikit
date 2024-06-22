9106199bf8be6786d0bc4f056389226a3309a38d
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-06-22 22:12:46 +0800

    init

---
+++ src/components/button/UButton.stories.ts
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+import UButton from './UButton.vue';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+const meta = {
+  title: 'Components/Button',
+  component: UButton,
+  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  argTypes: {
+    type: {control: 'select', options: ['button', 'reset', 'submit']}
+  },
+} satisfies Meta<typeof UButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/api/csf
+ * to learn how to use render functions.
+ */
+export const Default: Story = {
+  args: {
+  },
+};
+
+export const Fds: Story = {
+  args: {
+    label: "fdsfdsfsdfdsfdsfdsfdsfds"
+  }
+};
+
---
+++ src/components/button/UButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button :type="props.type">
+    {{props.label}}
+  </button>
+</template>
+
+<script setup lang="ts">
+import {ButtonHTMLAttributes} from "vue";
+
+interface Props {
+  type?: ButtonHTMLAttributes['type'];
+  label: string
+}
+
+const props = withDefaults(defineProps<Props>(), {type: 'button', label: 'Label'});
+</script>
+
+<style scoped>  
+</style>
